07c60bb332f01c4273a6586136e760a08ecf3d42
barm4ley <mamu@Totenkopf.local>
2015-06-12 16:25:45 +0300

    Added manifest for barm4ley user

---
+++ modules/people/manifests/barm4ley.pp
@@ -0,0 +1,48 @@
+class people::barm4ley {
+
+  include iterm2::dev
+  include slate
+  include wget
+  include ohmyzsh
+
+  include chrome
+  include dropbox
+  include flowdock
+  include skype
+  include vim
+
+
+  include virtualbox
+  include vagrant
+
+  #
+  # Remove services we don't want
+  #
+  service {"dev.nginx":
+      ensure => "stopped",
+  }
+
+  service {"dev.dnsmasq":
+      ensure => "stopped",
+  }
+
+  #$my_home  = "/Users/${::luser}"
+  #$projects = "${my_home}/vagrant/code/rozza"
+
+  #file { $projects:
+    #ensure => directory,
+  #}
+
+  #$dotfiles = "${projects}/dotfiles"
+
+  #repository { $dotfiles:
+    #source  => 'rozza/dotfiles',
+    #require => File[$projects],
+    #notify  => Exec['rozza-make-dotfiles'],
+  #}
+
+  #exec { 'rozza-make-dotfiles':
+    #command     => "cd ${dotfiles} && make",
+    #refreshonly => true,
+  #}
+}
